1c71e912c71131ce026d658470afdb48c32979b6
Mark Burns <markthedeveloper@gmail.com>
2016-02-21 19:50:51 +0000

    refactor

--- lib/file_tree/traversal_calculator.rb
+++ lib/file_tree/traversal_calculator.rb
@@ -4,12 +4,8 @@ require_relative "relation"
 module FileTree
   class TraversalCalculator
     def traversals_for(a, b)
-      return handle_both_files(a,b) if a.file? && b.file?
-      return handle_both_directories(a,b) if a.directory? && b.directory?
-      return handle_file_and_directory(a,b) if a.file? && b.directory?
       return handle_directory_and_file(a,b) if a.directory? && b.file?
-
-      raise NotImplementedError
+      handle_both_files(a, b)
     end
 
     class Internal < Struct.new(:a,:b)
@@ -31,46 +27,10 @@ module FileTree
       end
     end
 
-    def handle_file_and_directory(a, b)
-      if a.dirname == b.dirname
-        return Relation
-      end
-
-      if (a + ".." == b)
-        return Relation
-      end
-
-      if (b + ".." == a)
-        return Relation
-      end
-    end
-
     def handle_directory_and_file(a,b)
-      if a.dirname == b.dirname
-        return Relation
-      end
+      return ChildFile if (b + ".." == a)
 
-      if (a + ".." == b)
-        return Relation
-      end
-
-      if (b + ".." == a)
-        return ChildFile
-      end
-    end
-
-    def handle_both_directories(a,b)
-      if a.dirname ==b.dirname
-        return Relation
-      end
-      if (a + ".." == b)
-        return Relation
-      end
-
-      if (b + ".." == a)
-        return Relation
-      end
+      Relation
     end
   end
-
 end
